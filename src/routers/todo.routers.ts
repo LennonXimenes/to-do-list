193bfe579ff78ee33ff3658fdc77a2b81c4eeb30
Lennon <lennon_ximenes@hotmail.com>
2024-05-03 10:58:57 -0300

    fix - routers

--- src/routers/todo.routers.ts
+++ src/routers/todo.routers.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import todoControllers from "../controllers/todo.controllers";
-import { validateBody, validateIdExists, validateTitleExists, validateUsernameExists, verifyToken, verifyUserPermission, verifyUserTodoPermission } from "../middlewares";
-import { todoCreateSchema, todoUpdateSchema } from "../schemas";
+import { validateBody, validateTitleExists, verifyToken, verifyUserPermission, verifyUserTodoPermission } from "../middlewares";
+import { todoUpdateSchema } from "../schemas";
 
 const todoRouter: Router = Router();
 
 todoRouter.post("/:userId", verifyToken, verifyUserPermission, validateTitleExists, todoControllers.createTodo);
 todoRouter.get("", todoControllers.readTodo);
 todoRouter.patch("/:todoId", verifyToken, verifyUserTodoPermission, validateBody(todoUpdateSchema), todoControllers.updateTodo);
-todoRouter.delete("/:todoId", verifyToken, verifyUserPermission, todoControllers.deleteTodo);
+todoRouter.delete("/:todoId", verifyToken, verifyUserTodoPermission, todoControllers.deleteTodo);
 
 export default todoRouter;
